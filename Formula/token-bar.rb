590456fba4fcb5f43e5bef99986ace7881a21310
Shrivara <shrivara@users.noreply.github.com>
2026-07-17 16:06:18 +0000

    token-bar: add bottles

--- Formula/token-bar.rb
+++ Formula/token-bar.rb
@@ -5,6 +5,14 @@ class TokenBar < Formula
   sha256 "0bfd282acfc3dcb46999eefba1e655c10fbef2fa94c4300d37332e9fdd30a759"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/shrivara/homebrew-tap/releases/download/bottles-token-bar-0.7.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "759403b7bbdc4ce64b8e223e96b562afb13305c26b19fada206bade6ed5d4c91"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a9ba1ab78540b80c7bf2ff312bcbe062afc65cf64c174ac8c37bd06151acb8d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8b552f2fb4994bd8dd175d713b032e5a3193b4d5dbafb4aba9f27306204058f"
+  end
+
 
 
 
