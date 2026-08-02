d61b85b7d11ff20a035013571ac573e20e383d5e
Shrivara <shrivara@users.noreply.github.com>
2026-08-02 18:12:02 +0000

    token-bar: add bottles

--- Formula/token-bar.rb
+++ Formula/token-bar.rb
@@ -5,6 +5,14 @@ class TokenBar < Formula
   sha256 "3517d78d66d548db0e40231d5645b66b6fab846eea3b35d1d5ae3c575f7d9327"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/shrivara/homebrew-tap/releases/download/bottles-token-bar-0.8.2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a0332f94b0dc28e85e44df7d91ea68f4f1b94ffc8b92ddf40b4669215e06216b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4da1d19728cc11f63cb84536683007f0f16b380b0a6324036213236c9006e766"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "935caf738f016f9c8cbbdcd4b3eacb7a2f0144b834821bd69796f65ff94b5070"
+  end
+
 
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox"
